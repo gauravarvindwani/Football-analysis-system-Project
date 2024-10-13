a7e4eaa275f628ad945913e336193c58dde3e9e4
vishalkumar3259 <vishpra07@gmail.com>
2024-10-13 16:19:28 +0530

    drawn refree

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -4,6 +4,8 @@ import pickle
 import cv2
 import os
 import sys
+import math
+import numpy as np
 sys.path.append('../')
 from utils import get_center_of_bbox, get_bbox_width
 
@@ -100,6 +102,66 @@ class Tracker:
             lineType=cv2.LINE_4
         )
 
+        rectangle_width = 40
+        rectangle_height=20
+        x1_rect = x_center - rectangle_width//2
+        x2_rect = x_center + rectangle_width//2
+        y1_rect = (y2- rectangle_height//2) +15
+        y2_rect = (y2+ rectangle_height//2) +15
+
+        if track_id is not None:
+            cv2.rectangle(frame,
+                          (int(x1_rect),int(y1_rect) ),
+                          (int(x2_rect),int(y2_rect)),
+                          color,
+                          cv2.FILLED)
+            
+            x1_text = x1_rect+12
+            if track_id > 99:
+                x1_text -=10
+            
+            cv2.putText(
+                frame,
+                f"{track_id}",
+                (int(x1_text),int(y1_rect+15)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (255,255,255),
+                2
+            )
+    
+    # def draw_hexagon(self, frame, bbox, color, track_id=None):
+    #     y2 = int(bbox[3])
+    #     x_center, _ = get_center_of_bbox(bbox)
+    #     width = get_bbox_width(bbox)
+    #     size = int(width / 2)
+    
+    #     hexagon_points = []
+    #     for i in range(6):
+    #         angle = math.radians(i * 60)
+    #         x = int(x_center + size * math.cos(angle))
+    #         y = int(y2 + size * math.sin(angle))
+    #         hexagon_points.append((x, y))
+        
+    #         hexagon_points = [(int(x), int(y)) for x, y in hexagon_points]
+    #         cv2.polylines(frame, [np.array(hexagon_points, dtype=np.int32)], isClosed=True, color=color, thickness=2, lineType=cv2.LINE_4)
+
+        return frame
+    
+    def draw_traingle(self,frame,bbox,color):
+        y= int(bbox[1])
+        x,_ = get_center_of_bbox(bbox)
+
+        triangle_points = np.array([
+            [x,y],
+            [x-10,y-20],
+            [x+10,y-20],
+        ])
+        #For the shape
+        ##For the border
+        cv2.drawContours(frame, [triangle_points],0,color, cv2.FILLED)
+        cv2.drawContours(frame, [triangle_points],0,(0,0,0), 2)
+
         return frame
 
     def draw_annotations(self,video_frames, tracks):
@@ -115,6 +177,18 @@ class Tracker:
             for track_id, player in player_dict.items():
                 # color = player.get("team_color",(0,0,255))
                 frame = self.draw_ellipse(frame, player["bbox"],(0,0,255), track_id)
+            #Draw Refree
+            for track_id, referee in referee_dict.items():
+                # color = player.get("team_color",(0,0,255))
+                frame = self.draw_ellipse(frame, referee["bbox"],(0,0,0), track_id)
+             # Draw ball 
+            for track_id, ball in ball_dict.items():
+                frame = self.draw_traingle(frame, ball["bbox"],(0,0,255))
+
+
+            # Draw Team Ball Control
+           # frame = self.draw_team_ball_control(frame, frame_num, team_ball_control)
+
 
             output_video_frames.append(frame)
         
