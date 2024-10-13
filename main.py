3abe53bfb90256ff3fd0b7242794d67ded79a763
vishalkumar3259 <vishpra07@gmail.com>
2024-10-13 19:32:57 +0530

    update main

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from utils import read_video, save_video
 from trackers import Tracker
 # import cv2
-# import numpy as np
-# from team_assigner import TeamAssigner
-# from player_ball_assigner import PlayerBallAssigner
-# from camera_movement_estimator import CameraMovementEstimator
-# from view_transformer import ViewTransformer
-# from speed_and_distance_estimator import SpeedAndDistance_Estimator
+
 
 
 def main():
