c4900969a98336c6921121bb71533b59fca0b250
gauravarvindwani <wanigaurav2705@gmail.com>
2024-10-16 16:45:11 +0530

    cropped image

--- main.py
+++ main.py
@@ -18,7 +18,17 @@ def main():
 
     tracks = tracker.get_object_tracks(video_frames,read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
+    
+    #save cropped image of a player
+    for track_id, player in tracks['players'][0].items():
+        bbox = player['bbox']
+        frame = video_frames[0]
 
+        # crop bbox from frame
+        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+        
+        # save the croppd image
+        cv2.imwrite(f'output_videos/cropped_image.jpg', cropped_image)
     # Draw output 
     ## Draw object Tracks
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
